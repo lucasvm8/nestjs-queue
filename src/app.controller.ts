c1c122bcc5efa3c87666d690d3e166b174d795fd
lucasvm8 <lucasvm8@gmail.com>
2022-04-07 12:07:51 -0300

    add: messages api

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+// decorator para tornálo um controller
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MessagesModule } from './messages/messages.module';
 
+//Decorator
 @Module({
-  imports: [],
+  imports: [MessagesModule],
   controllers: [AppController],
   providers: [AppService],
 })
